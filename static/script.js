7a42b5c726fc044487d8d6bf53096246a05f2485
trung24 <trung24499@gmail.com>
2025-02-17 05:24:04 +0300

    Add some effects to the page

--- static/script.js
+++ static/script.js
@@ -1,12 +1,27 @@
 const musicToggle = document.getElementById('music-toggle');
-        const backgroundMusic = document.getElementById('background-music');
+const backgroundMusic = document.getElementById('background-music');
 
-        musicToggle.addEventListener('click', () => {
-            if (backgroundMusic.paused) {
-                backgroundMusic.play();
-                musicToggle.textContent = 'Pause Music';
-            } else {
-                backgroundMusic.pause();
-                musicToggle.textContent = 'Play Music';
-            }
-        });
+musicToggle.addEventListener('click', () => {
+    if (backgroundMusic.paused) {
+        backgroundMusic.play();
+        musicToggle.textContent = 'Pause Music';
+    } else {
+        backgroundMusic.pause();
+        musicToggle.textContent = 'Play Music';
+    }
+});
+
+const profilePicture = document.getElementById('profile-picture');
+const modal = document.getElementById('image-modal');
+
+profilePicture.addEventListener('click', () => {
+    modal.style.display = 'flex';
+    document.getElementsByClassName("profile-container").style.opacity = '0.5';
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+        document.getElementsByClassName("profile-container").style.opacity = '1';
+    }
+});
